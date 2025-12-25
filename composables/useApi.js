534fc0e5bd0b3fe99fd57d788252fc969eafd5dd
Mohamed Ramadan <@mramadan5080>
2025-12-25 12:46:56 +0200

    update

--- composables/useApi.js
+++ composables/useApi.js
@@ -3,11 +3,16 @@ import axios from "axios";
 export const useApi = () => {
   const baseURL = "https://reevent-sa.com/api/";
 
+  const headers = {
+    "x-api-key": "5f43766dcd92b8c3e7639d2a8791063c",
+  };
+
   if(process.client){
-    axios.defaults.headers.lang = localStorage.getItem("locale") || "ar";
+    headers.lang = localStorage.getItem("locale") || "ar";
   }
 
   return axios.create({
     baseURL,
+    headers,
   });
 };
